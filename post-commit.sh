35ce9bb87f23680eebd35cb0f501cc7285835046
Jonah Langlieb <jonahlanglieb@gmail.com>
2015-10-24 00:14:27 -0400

    Init

--- post-commit.sh
+++ post-commit.sh
@@ -1,4 +1,4 @@
-git subtree push --prefix _site/ origin master;
+git subtree push --prefix _site origin master;
 notify-send -i face-laugh "Sending Out"
 # ln -s ../../post-commit.sh .git/hooks/post-commit
 
