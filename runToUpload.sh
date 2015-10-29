701b6096cea122920a2021cb5c87e372a0f0ba12
jzl003 <jonahmail1@gmail.com>
2015-10-29 22:49:41 +0000

    Init BookGraph

--- runToUpload.sh
+++ runToUpload.sh
@@ -2,3 +2,4 @@
 jekyll build;
 cp BookGraph.html _site/
 git subtree push --prefix _site origin master;
+# git init;git remote add origin https://github.com/MDJCL/MDJCL.github.io.git;git add *;git commit -m "`date`";git push -f -u origin master
